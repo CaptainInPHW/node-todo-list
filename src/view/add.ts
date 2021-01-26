8f6c46368706b7ed8b0ba12b6b80e1b527702b16
CaptainOfPhB <captaininphw@gmail.com>
2021-01-27 00:19:48 +0800

    add sub command

--- src/view/add.ts
+++ src/view/add.ts
@@ -1,27 +1,50 @@
 // @ts-ignore
+const chalk = require('chalk');
+// @ts-ignore
 const { Command } = require('commander');
 // @ts-ignore
 const program = new Command();
 // @ts-ignore
 const TaskController = require('../controller/task');
 
+/**
+ *  Add a new task by default
+ */
 program
-  .option('-a, --all', 'List all items')
-  .option('-v, --verbose', 'List all items with detail info')
+  .action(() => {
+    console.log('Add a new task by default');
+  });
+
+/**
+ *  Add a new tag
+ */
+program
+  .command('tag <tag_name>')
+  .description(chalk.underline.bold('Add a new tag'))
+  .action(() => {
+    const [, ...rest] = program.args;
+    const tagName = rest.join(' ');
+    console.log(tagName);
+  })
   .addHelpText('after', `
-Example:
-  $ st list
-  $ st ls
-  $ st list -a
-  $ st list -av
-  $ st list --all
-  $ st list --all --verbose
+${chalk.bgGreen.bold.italic(' Example ')}
+  $ ${chalk.greenBright('st')} add tag ${chalk.bold.underline('shopping')}
+  $ ${chalk.greenBright('st')} add tag ${chalk.bold.underline('\'useful skill\'')}
+  `);
+
+/**
+ *  Add a new group
+ */
+program
+  .command('group <group_name>')
+  .description(chalk.underline.bold('Add a new group'))
+  .action((groupName: string) => {
+    console.log(groupName);
+  })
+  .addHelpText('after', `
+${chalk.bgGreen.bold.italic(' Example ')}
+  $ ${chalk.greenBright('st')} add group ${chalk.bold.underline('work')}
+  $ ${chalk.greenBright('st')} add group ${chalk.bold.underline('shopping')}
   `);
 
 program.parse(process.argv);
-
-//@ts-ignore
-const opts = program.opts();
-
-console.log(opts);
-console.log(TaskController.get());
